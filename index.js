a7ee71de9edc5dd9a309b4c589ac9290a08da750
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-03-03 13:59:26 -0500

    'Complete'

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
+
+//creates a function to get assignment types (keys) with the parameter assignments whivh is an object of assignments
 function getAssignmentTypes(assignments) {
 
-  
+  //create a variable called assignKeys 
+  //using the Object.keys method with pulls all the keys from assignments and assigns to the variable assignKeys
    let assignKeys = Object.keys(assignments)
+
+   //return the variable with all assignments keys inside
     return assignKeys
-  
 
 }
 
@@ -19,10 +23,17 @@ function getAssignmentTypes(assignments) {
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
+
+//creates a function to get the perscentage of score
 function getPercentageScore(score) {
 
-  let rawScore = (score.received / score.max) * 100
-  return Math.round(rawScore)
+  //creates a variable to hold the percentage score
+  //percentage is obtained by dividing the score recieved by the max score then multiplying by 100
+  //mathamatical operation () are used to define with operation to do first
+  let percentageScore = (score.received / score.max) * 100
+
+  //return the percentage score rounded to the nearest integer
+  return Math.round(percentageScore)
 
 }
 
@@ -34,22 +45,30 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
+
+//creates a function to get the highest of two scores with paramaters score1 and score2 which are objects
 function getHighestOfTwoScores(score1, score2) {
+
+  //create varibale to represent the high score of score1
   let highScore1 = 0; 
+  //creates a variable to represent the high score of score2
   let highScore2 = 0;
 
-  // if(score1.received > score2.received){
+  
+
+    //give high score 1 the value in perentage of score1
     highScore1 = (score1.received / score1.max) * 100
-  // } else {
+   //give high score 2 the value in perentage of score2
     highScore2 = (score2.received / score2.max) * 100
-  // }
+  
+    //conditional comparison if percentage version of score1 rounded to the nearest integer is greater than the percentage version of score2 
     if(Math.round(highScore1) > Math.round(highScore2)){
+      //return percentage version of score1 rounded to the nearest integer
       return Math.round(highScore1);
     }else{
+      //if not return percentage version of score2 rounded to the nearest integer
       return Math.round(highScore2);
     }
-
-
   
 }
 
